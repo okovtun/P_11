ef0ceb95519429d3a5c2ee189df7f1de6db3a674
Oleg <31523835+okovtun@users.noreply.github.com>
2022-05-16 19:03:26 +0300

    DataTypes day 2

--- DataTypes/DataTypes/main.cpp
+++ DataTypes/DataTypes/main.cpp
@@ -5,7 +5,24 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "Russian");
-	cout << "ѕривет!" << endl;
-	cout << true << endl;
-	cout << false << endl;
+	//cout << "ѕривет!" << endl;
+	//cout << true << endl;
+	//cout << false << endl;
+
+	/*cout << sizeof(int) << endl;
+	cout << INT_MIN << "\t" << INT_MAX << endl;
+	cout << 0 << "\t" << UINT_MAX << endl;*/
+
+	//float	 - вещественное число одинарной точности, 4B
+	//double - вещественное число двойной точности 8B
+	cout << sizeof(float) << endl;
+	cout << FLT_MIN << " ... " << FLT_MAX << endl;
+	cout << "\n----------------------\n";
+	cout << sizeof(double) << endl;
+	cout << DBL_MIN << " ... " << DBL_MAX << endl;
+
+
+	int _1stPlace;
+	int Price;
+	cout << price << endl;
 }
