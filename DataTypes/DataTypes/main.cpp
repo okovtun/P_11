e6a925936335146cbbe589d7d270b6345804012d
Oleg <31523835+okovtun@users.noreply.github.com>
2022-05-19 18:15:01 +0300

    Constants

--- DataTypes/DataTypes/main.cpp
+++ DataTypes/DataTypes/main.cpp
@@ -15,14 +15,45 @@ void main()
 
 	//float	 - вещественное число одинарной точности, 4B
 	//double - вещественное число двойной точности 8B
-	cout << sizeof(float) << endl;
+	/*cout << sizeof(float) << endl;
 	cout << FLT_MIN << " ... " << FLT_MAX << endl;
 	cout << "\n----------------------\n";
 	cout << sizeof(double) << endl;
-	cout << DBL_MIN << " ... " << DBL_MAX << endl;
+	cout << DBL_MIN << " ... " << DBL_MAX << endl;*/
 
-
-	int _1stPlace;
+	/*int _1stPlace;
 	int Price;
-	cout << price << endl;
+	cout << price << endl;*/
+
+	int speed;	//переменное значение
+	//проинициализировать
+	cout << "Введите текущую скорость: ";
+	cin >> speed;	//Ввод значения переменной с клавиатуры
+	//инициалиализация - это присвоение начального значения.
+	cout << "Ваша скорость: " << speed << " км/час" << endl;
+	const int MAX_SPEED = 250;//постоянное (константное) значение
+	//max_speed = 360;
+	5;	//числовая константа типа int
+	5l;	//числовая константа типа long
+	5ll;//числовая константа типа long long 
+	5.;	//числовая константа типа double
+	.5;	//числовая константа типа double
+	cout << sizeof(5.f) << endl;
+	cout << typeid(5.f).name() << endl;
+	cout << typeid(.5f).name() << endl;
+	cout << .5 << endl;
+	//http://cplusplus.com/doc/tutorial/constants/
+
+	cout << '+' << endl;
+	cout << sizeof('+') << endl;
+	cout << typeid('+').name() << endl;
+
+	cout << "Hello" << endl;
+	cout << sizeof("Hello") << endl;
+
+	3;
+	-3;		//Unary minus
+	8 - 3;	//Binary minus
+	8 * 3;	//Binary multiply
+	//*3;//Have no sence
 }
--- ReadMe.txt
+++ ReadMe.txt
@@ -5,6 +5,12 @@ https://meet.google.com/mho-khdw-fhj
 https://discord.gg/vcYaQu6c
 
 TODO:
+https://github.com/okovtun/P_11/blob/master/VariablesTask.pdf
+
+Constants:
+http://cplusplus.com/doc/tutorial/constants/
+
+TODO:
 ¬ывести в консоль стишок "¬ лесу родилась ®лочка".
 —тишок должен быть выровнен по середине окна консоли,
 куплеты должны быть разделены пустой строкой.
