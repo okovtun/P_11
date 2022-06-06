701fd986bfb4b88d26ac1f0cba8d6f5d97d89d36
Clayman <Clayman@DESKTOP-DM8E77U>
2022-06-06 18:48:28 +0300

    Loops.

---
+++ ControlStructures/Loops/main.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+#define Escape 27
+
+//#define WHILE_1
+//#define WHILE_2
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef WHILE_1
+	int n;		//Количество итераций
+	int i = 0;	//Счетчик цикла
+	cout << "Введите количество итераций: ";
+	cin >> n;
+	while (i < n)
+	{
+		cout << ++i << " Hello loops";
+		cout << 1024 << endl;
+		//i++;
+	}
+#endif // WHILE_1
+
+#ifdef WHILE_2
+	int n;
+	cout << "Введите количество итераций: "; cin >> n;
+	while (n--)
+	{
+		cout << n << " Hello World!" << endl;
+	}
+#endif // WHILE_2
+
+	char key;	//Эта переменная будет хранить код клавиши
+	do
+	{
+		key = _getch();	//Функция _getch() ожидает нажатие клавиши,
+		//и возвращает ASCII-код нажатой клавиши.
+		//Функция _getch() находится в библиотеке conio.h
+		cout << (int)key << "\t" << key << endl;
+		//(int)key - явное преобразование переменной key в тип данных int
+	} while (key != Escape);
+}
+
+//FOR - это цикл на заданное число итераций
+/*
+---------------------------------------------------
+		for(counter; condition; expression)
+		{
+			....;
+			group-of-statements;
+			....;
+		}
+---------------------------------------------------
+*/
