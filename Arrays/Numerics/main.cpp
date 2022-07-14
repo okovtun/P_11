0ba1a7e086ecf3b4f1ffd46ef0483be8ddda566b
Clayman <Clayman@DESKTOP-DM8E77U>
2022-07-14 17:26:37 +0300

    Dec2Bin

--- Arrays/Numerics/main.cpp
+++ Arrays/Numerics/main.cpp
@@ -5,8 +5,23 @@ void main()
 {
 	setlocale(LC_ALL, "");
 	const int n = 32;	//максимально возможная разрядность двоичного числа
-	bool bin[n] = {};
+	bool bin[n] = {};	//в этот массив будем сохранять разряды двоичного числа
 	int decimal;		//десятичное число, введенное пользователем
 	cout << "Введите десятичное число: "; cin >> decimal;
-
+	int i = 0;	//номер разряда (номер элемента массива)
+	/*while (decimal)
+	{
+		bin[i] = decimal % 2;
+		decimal /= 2;
+		i++;
+	}*/
+	for (; decimal; i++,decimal /= 2)
+	{
+		bin[i] = decimal % 2;
+	}
+	for (--i; i >= 0; i--)
+	{
+		cout << bin[i];
+	}
+	cout << endl;
 }
