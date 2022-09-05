6bd9b189c5b527570dcf4061b9ab3dd47719e7c7
Clayman <Clayman@DESKTOP-DM8E77U>
2022-09-05 19:24:56 +0300

    Functions day 1

---
+++ Functions/Arrays/main.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+#define tab "\t"
+
+void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
+void Print(int arr[], const int n);
+void Sort(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+
+	FillRand(arr, n);
+	Print(arr, n);
+	Sort(arr, n);
+	Print(arr, n);
+
+	//Too few arguments in function call, если передать меньше параметров, чем она принимает.
+	//Too many arguments in function call, если передать в нее больше параметров, чем она принимает.
+	//Function doesn't take N arguments
+
+	//Default arguments
+}
+
+void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100)
+{
+	//«аполн€ем массив случайными числами:
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+}
+void Print(int arr[], const int n)
+{
+	//¬ывод массива на экран:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+void Sort(int arr[], const int n)
+{
+	//—ортировка массива:
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
---
+++ Functions/Functions/main.cpp
@@ -0,0 +1,45 @@
+﻿//Functions
+#include<iostream>
+using namespace std;
+
+int Add(int a, int b);		//Прототип функции - Объявление функции (Function declaration)
+int Sub(int a, int b);
+int Mul(int a, int b);
+double Div(int a, int b);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a, b;
+	cout << "Введите два числа: "; cin >> a >> b;
+
+	int c = Add(a, b);	//Вызов функции (Function call) - использование функции
+	
+	cout << a << " + " << b << " = " << c << endl;
+	cout << a << " - " << b << " = " << Sub(a, b) << endl;
+	cout << a << " * " << b << " = " << Mul(a, b) << endl;
+	cout << a << " / " << b << " = " << Div(a, b) << endl;
+}
+
+//type name(parameters)
+int Add(int a, int b)	//Реализация функции - определение функции (Function definition)
+{
+	//Addition - сложение
+	int sum = a + b;
+	return sum;
+}
+int Sub(int a, int b)
+{
+	//Subtraction - вычитание
+	return a - b;
+}
+int Mul(int a, int b)
+{
+	//Multiplication - умножение
+	return a * b;
+}
+double Div(int a, int b)
+{
+	//Division - деление
+	return (double)a / b;
+}
